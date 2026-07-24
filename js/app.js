6060a5ac1bf900cbb922c6d87dbe830c9162a3f5
Mahesh Milan <dev.mahesh.milan@gmail.com>
2026-07-24 15:00:11 +0530

    redesign

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 window.onload = () => {
     window.scrollTo(0, 0);
 };
+
---
+++ js/projects.js
@@ -0,0 +1,43 @@
+const projects = [
+
+    {
+        title: "Explore Sri Lanka",
+        type: "Tourism Platform",
+        description: "Modern tourism platform with booking, travel guides and local attractions.",
+
+        image: "images/hero-bg.png",
+
+        technologies: [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+
+        status: "Completed",
+
+        github: "#",
+
+        demo: "#"
+    },
+
+    {
+        title: "DevOS Portfolio",
+        type: "Personal Portfolio",
+        description: "Operating System inspired developer portfolio.",
+
+        image: "images/hero-bg.png",
+
+        technologies: [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+
+        status: "In Progress",
+
+        github: "#",
+
+        demo: "#"
+    }
+
+];
